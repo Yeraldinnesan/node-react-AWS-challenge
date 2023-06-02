d9f2d1e9bd1f06733043ff6424b73453d4d7ceb0
Yeraldinnesan <ysanabria92@hotmail.com>
2023-06-01 20:36:55 -0500

    create user functioning

--- server/src/controllers/user/createUser.js
+++ server/src/controllers/user/createUser.js
@@ -1,3 +1,31 @@
+import User from "../../models/user.js";
+
 export const createUser = async (req, res) => {
-  console.log("hello world");
+  try {
+    const { fullName, placeOfBirth } = req.body;
+
+    if (!fullName || !placeOfBirth) {
+      throw new Error("Full Name and place of birth are required");
+    }
+
+    const { country, state, city } = placeOfBirth;
+
+    if (!country || !state || !city) {
+      throw new Error("Country, state and city fields are required");
+    }
+
+    const newUser = new User({
+      fullName,
+      placeOfBirth: {
+        country,
+        state,
+        city,
+      },
+    });
+    const createdUser = await newUser.save();
+    res.status(201).json(createdUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error while creating the user" });
+  }
 };
