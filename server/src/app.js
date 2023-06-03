4a45996e048d95efd332b33882763cdcaa2d3ba3
Yeraldinnesan <ysanabria92@hotmail.com>
2023-06-03 02:21:55 -0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,25 +3,24 @@ import cors from "cors";
 import morgan from "morgan";
 
 import routes from "./routes/index.js";
-import path from "path";
-
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/dist");
-
 const app = express();
+// import path from "path";
 
-app.use(express.static(buildPath));
+// const _dirname = path.dirname("");
+// const buildPath = path.join(_dirname, "../client/dist");
 
-app.get("/", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/dist/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+// app.use(express.static(buildPath));
+
+// app.get("/", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "../client/dist/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
 app.use(cors());
 app.use(express.json());
